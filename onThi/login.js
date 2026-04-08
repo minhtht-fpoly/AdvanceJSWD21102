5284bdc0a0b36126f315aec48631bb4a2a9f291b
minhtht-fpoly <minhtht@fpt.edu.vn>
2026-04-08 18:59:53 +0700

    Login register

--- onThi/login.js
+++ onThi/login.js
@@ -1,21 +1,29 @@
-async function loginAccount() {
-  try {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    const respon = await fetch("http://localhost:3000/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
+async function loginAccount(e) {
+  e.preventDefault();
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  axios
+    .post("http://localhost:3000/login", { email: email, password: password })
+    .then((res) => {
+      console.log(res);
+      alert("Đăng nhập thành công");
+      localStorage.setItem("accessToken", res.data.accessToken);
+    })
+    .catch((err) => {
+      console.log(err);
     });
-    alert("Đăng nhập thành công");
-    localStorage.setItem("accessToken", respon.data.accessToken);
-  } catch (error) {
-    alert("Đăng nhập thất bại");
-  }
+
+  // fetch("http://localhost:3000/login", {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify({ email: email, password: password }),
+  // })
+  //   .then((res) => {
+  //     if (res.ok) {
+  //       alert("Đăng nhập thành công");
+  //     }
+  //   })
+  //   .catch((err) => alert("Đăng nhập thất bái"));
 
   //   window.location.href = "index.html";
 }
--- onThi/register.js
+++ onThi/register.js
@@ -2,13 +2,13 @@ async function registerAccount() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const respon = await fetch("http://localhost:3000/register", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
+  axios
+    .post("http://localhost:3000/register", {
       email: email,
       password: password,
-    }),
-  });
+    })
+    .then((res) => {
+      console.log(res);
+    });
   //   window.location.href = "index.html";
 }
